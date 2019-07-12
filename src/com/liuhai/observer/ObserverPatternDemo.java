38407689bdd1d4e6e2693751604a3b33fe3367b5
asus <asus@DESKTOP-U322PR6>
2019-07-12 09:24:31 +0800

    观察者模式

---
+++ src/com/liuhai/observer/ObserverPatternDemo.java
@@ -0,0 +1,17 @@
+package com.liuhai.observer;
+
+//观察者模式
+public class ObserverPatternDemo {
+	public static void main(String[] args) {
+		Subject subject=new Subject();
+		
+		new HexaObserver(subject);
+		new OctalObserver(subject);
+		new BinaryObserver(subject);
+		
+		 System.out.println("First state change: 15");   
+	     subject.setState(15);
+	     System.out.println("Second state change: 10");  
+	     subject.setState(10);
+	}
+}
