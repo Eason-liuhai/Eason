a92491c57fb3692f0d8803542a0f26e5803fbe54
asus <asus@DESKTOP-U322PR6>
2019-07-12 09:20:54 +0800

    中介者模式

---
+++ src/com/liuhai/mediator/MediatorPatternDemo.java
@@ -0,0 +1,12 @@
+package com.liuhai.mediator;
+
+//中介者模式
+public class MediatorPatternDemo {
+	public static void main(String[] args) {
+		User robert=new User("Robert");
+		User john=new User("John");
+		
+		robert.sendMessage("Hi!John!");
+		john.sendMessage("Hello!Robert!");
+	}
+}
