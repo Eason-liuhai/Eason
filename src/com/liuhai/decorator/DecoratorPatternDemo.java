2daeecc70d0e806db200d2b1b11064e4ef6c6352
asus <asus@DESKTOP-U322PR6>
2019-07-12 09:15:26 +0800

    装饰器模式

---
+++ src/com/liuhai/decorator/DecoratorPatternDemo.java
@@ -0,0 +1,21 @@
+package com.liuhai.decorator;
+
+
+//×°ÊÎÆ÷Ä£Ê½
+public class DecoratorPatternDemo {
+	public static void main(String[] args) {
+		Shape circle=new Circle();
+		
+		ShapeDecorator redCircle=new RedShapeDecorator(new Circle());
+		ShapeDecorator redRectangle=new RedShapeDecorator(new Rectangle());
+		
+		System.out.println("Circle with normal border");
+		circle.draw();
+		
+		System.out.println("\nCircle of red border");
+		redCircle.draw();
+		
+		System.out.println("\nRectangle of red border");
+		redRectangle.draw();
+	}
+}
