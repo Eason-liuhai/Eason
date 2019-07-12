f6dd168bcad641330ebcfda7f29fa0626ebce6cd
asus <asus@DESKTOP-U322PR6>
2019-07-12 09:19:06 +0800

    数据访问对象模式

---
+++ src/com/liuhai/dataAccess/DaoPatternDemo.java
@@ -0,0 +1,26 @@
+package com.liuhai.dataAccess;
+
+
+//数据访问对象模式
+public class DaoPatternDemo {
+	   public static void main(String[] args) {
+	      StudentDao studentDao = new StudentDaoImpl();
+	 
+	      //输出所有的学生
+	      for (Student student : studentDao.getAllStudents()) {
+	         System.out.println("Student: [RollNo : "
+	            +student.getRollNo()+", Name : "+student.getName()+" ]");
+	      }
+	 
+	 
+	      //更新学生
+	      Student student =studentDao.getAllStudents().get(0);
+	      student.setName("Michael");
+	      studentDao.updateStudent(student);
+	 
+	      //获取学生
+	      studentDao.getStudent(0);
+	      System.out.println("Student: [RollNo : "
+	         +student.getRollNo()+", Name : "+student.getName()+" ]");      
+	   }
+	}
